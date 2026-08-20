4140753163052c76d06d8c3cab0fb724ae2a81ae
Edrich Yu <edrichyu15269@gmail.com>
2026-08-20 13:52:03 +0800

    Add support for echo

--- src/main/java/Lobby.java
+++ src/main/java/Lobby.java
@@ -1,7 +1,14 @@
+import java.util.Scanner;
+
 /**
  * Starts the Lobby chatbot application.
  */
 public class Lobby {
+    /**
+     * Displays a greeting, echoes each user command, and ends when the user enters {@code bye}.
+     *
+     * @param args command-line arguments (not used)
+     */
     public static void main(String[] args) {
         String banner = " _           _     _          \n"
                 + "| |    ___  | |__ | |__  _   _\n"
@@ -16,7 +23,16 @@ public class Lobby {
         System.out.println("Hello! I'm Lobby.");
         System.out.println("What can I do for you?");
         System.out.println(divider);
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println(divider);
+
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            String command = scanner.nextLine();
+            System.out.println(divider);
+
+
+
+            System.out.println(" " + command);
+            System.out.println(divider);
+        }
     }
 }
