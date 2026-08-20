865bce47416b36b671d355680f9db9325f8e2ba0
Edrich Yu <edrichyu15269@gmail.com>
2026-08-20 23:08:24 +0800

    Add mark command

--- src/main/java/Lobby.java
+++ src/main/java/Lobby.java
@@ -26,6 +26,7 @@ public class Lobby {
 
         Scanner scanner = new Scanner(System.in);
         String[] tasks = new String[100];
+        boolean[] taskDone = new boolean[100];
         int taskCount = 0;
         while (true) {
             String command = scanner.nextLine();
@@ -36,10 +37,27 @@ public class Lobby {
                 System.out.println(divider);
                 break;
             } else if (command.equals("list")) {
+                System.out.println(" Here are the tasks in your list:");
                 for (int i = 0; i < taskCount; i++) {
-                    System.out.println(" " + (i + 1) + ". " + tasks[i]);
+                    String status = taskDone[i] ? "[X]" : "[ ]";
+                    System.out.println(" " + (i + 1) + "." + status + " " + tasks[i]);
                 }
-            }else {
+            } else if (command.startsWith("mark ")) {
+                String taskNumberText = command.substring("mark ".length()).trim();
+                try {
+                    int taskNumber = Integer.parseInt(taskNumberText);
+                    if (taskNumber < 1 || taskNumber > taskCount) {
+                        System.out.println(" Please enter the number of a task in the list.");
+                    } else {
+                        int taskIndex = taskNumber - 1;
+                        taskDone[taskIndex] = true;
+                        System.out.println(" Nice! I've marked this task as done:");
+                        System.out.println("   [X] " + tasks[taskIndex]);
+                    }
+                } catch (NumberFormatException e) {
+                    System.out.println(" Please use mark followed by a task number.");
+                }
+            } else {
                 tasks[taskCount] = command;
                 taskCount++;
                 System.out.println(" added: " + command);
