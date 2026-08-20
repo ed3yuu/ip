6baf14c0760d73e6b64afb6e98c23adca3ee2775
Edrich Yu <edrichyu15269@gmail.com>
2026-08-20 10:58:42 +0800

    Update names to Lobby

---
+++ src/main/java/Lobby.java
@@ -0,0 +1,14 @@
+/**
+ * Starts the Lobby chatbot application.
+ */
+public class Lobby {
+    public static void main(String[] args) {
+        String banner = " _           _     _          \n"
+                + "| |    ___  | |__ | |__  _   _\n"
+                + "| |   / _ \\ | '_ \\| '_ \\| | | |\n"
+                + "| |__| (_) | |_) | |_) | |_| |\n"
+                + "|_____\\___/|_.__/|_.__/ \\__, |\n"
+                + "                         |___/";
+        System.out.println(banner);
+    }
+}
